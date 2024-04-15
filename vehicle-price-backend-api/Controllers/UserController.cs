cfeeb390c166de23526b690bd55ce10c36b31c5a
sanjana005 <sanjananimradha615@gmail.com>
2024-04-16 00:25:06 +0530

    User Login and Registration

--- vehicle-price-backend-api/Controllers/UserController.cs
+++ vehicle-price-backend-api/Controllers/UserController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Text;
+using vehicle_price_backend_api.Data;
+using vehicle_price_backend_api.Models;
 
 namespace vehicle_price_backend_api.Controllers
 {
@@ -6,9 +11,54 @@ namespace vehicle_price_backend_api.Controllers
     [Route("api/[controller]")]
     public class UserController : Controller
     {
-        public IActionResult InGetdex()
+        public static string Key = "ad##frw$";
+        private readonly UserAPIDbCon dbContext;
+
+        public UserController(UserAPIDbCon dbContext)
         {
-            return View();
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost("UserRegistartion")]
+        public async Task<IActionResult> UserRegistration(UserRegistrationDTO userRegistrationDTO)
+        {
+            var user = new User()
+            {
+                Email = userRegistrationDTO.Email,
+                Name = userRegistrationDTO.Name,
+                Password = ConvertToEncrypt(userRegistrationDTO.Password),
+                PhoneNo = userRegistrationDTO.PhoneNo,
+                UserType = userRegistrationDTO.UserType
+            };
+
+            await dbContext.Users.AddAsync(user);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpPost("UserLogin")]
+        public async Task<IActionResult> UserLogin(UserLoginDTO userLoginDTO)
+        {
+            var user = await dbContext.Users
+                .FirstOrDefaultAsync(u => u.Email == userLoginDTO.Email && u.Password == ConvertToEncrypt(userLoginDTO.Password));
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+
+        }
+
+        public static string ConvertToEncrypt(string password)
+        {
+            if (string.IsNullOrEmpty(password)) return "";
+            password += Key;
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            return Convert.ToBase64String(passwordBytes);
+
         }
     }
 }
---
+++ vehicle-price-backend-api/Models/UserRegistrationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace vehicle_price_backend_api.Models
+{
+    public class UserRegistrationDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNo { get; set; }
+        public string UserType { get; set; }
+    }
+}
