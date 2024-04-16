14f36f963402263a35be6e4c3fe80fa1680d55ae
sanjana005 <sanjananimradha615@gmail.com>
2024-04-16 09:49:11 +0530

    Created Vehicle model class and Db Context class

---
+++ vehicle-price-backend-api/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace vehicle_price_backend_api.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ vehicle-price-backend-api/Data/VehicleAPIDbCon.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vehicle_price_backend_api.Models;
+
+namespace vehicle_price_backend_api.Data
+{
+    public class VehicleAPIDbCon : DbContext
+    {
+        public VehicleAPIDbCon(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
