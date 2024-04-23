c88608370aa93c555892cb6c457de98e248d5eca
sanjana005 <sanjananimradha615@gmail.com>
2024-04-23 22:34:45 +0530

    Changes to controllers and database table changes

---
+++ vehicle-price-backend-api/Migrations/20240423074415_RemoveImageDataColumn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vehicle_price_backend_api.Migrations
+{
+    public partial class RemoveImageDataColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageData",
+                table: "Vehicles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageData",
+                table: "Vehicles",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
